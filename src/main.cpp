ae507c9ffd6f90be5fe8248ad9bd5f1aa3e6f60f
roman656 <romanstrah@mail.ru>
2026-01-02 17:17:36 +0300

    feat: added cli args parser

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
+#include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <string>
+
+#include <CLI/CLI.hpp>
 
 #include <TelemetryViewer/Telemetry/TelemetryParser.hpp>
 
@@ -7,12 +11,23 @@ using namespace TelemetryViewer;
 
 int main(int argc, char* argv[])
 {
-    const std::string telemetryFilename = "Data.txt";
-    std::ifstream telemetryFile { telemetryFilename };
+    const std::string appName = "TelemetryViewer";
+    const std::string appVersion = appName + " " + TELEMETRY_VIEWER_VERSION;
+    std::filesystem::path telemetryFilePath;
+    CLI::App app;
+
+    app.get_formatter()->column_width(40);
+    app.description("TelemetryViewer, a GUI viewer that plots parameter values from telemetry file");
+    app.set_version_flag("-v,--version", appVersion, "Print version info and exit");
+    app.add_option("telemetry_file", telemetryFilePath, "Path to telemetry file")->required()->check(CLI::ExistingFile);
+
+    CLI11_PARSE(app, argc, argv);
+
+    std::ifstream telemetryFile { telemetryFilePath };
 
     if (!telemetryFile.is_open())
     {
-        std::cerr << "[main]: failed to open file \"" << telemetryFilename << "\". Exiting..." << std::endl;
+        std::cerr << "[main]: failed to open file \"" << telemetryFilePath.string() << "\". Exiting..." << std::endl;
         return 1;
     }
 
