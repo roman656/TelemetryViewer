b0129649d5e5b0bb755249253d2ba2a7705d6ce8
roman656 <romanstrah@mail.ru>
2026-01-02 17:50:27 +0300

    ref: updated app description, CLI parser configuration refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,37 @@
-#include <filesystem>
-#include <fstream>
-#include <iostream>
-#include <string>
-
 #include <CLI/CLI.hpp>
 
 #include <TelemetryViewer/Telemetry/TelemetryParser.hpp>
 
 using namespace TelemetryViewer;
 
-int main(int argc, char* argv[])
+namespace
+{
+
+void ConfigureCliParser(CLI::App& cliParser)
 {
     const std::string appName = "TelemetryViewer";
     const std::string appVersion = appName + " " + TELEMETRY_VIEWER_VERSION;
+
+    cliParser.get_formatter()->column_width(40);
+    cliParser.description("TelemetryViewer is a GUI tool for plotting parameter values from telemetry files");
+    cliParser.set_version_flag("-v,--version", appVersion, "Print version info and exit");
+}
+
+}
+
+int main(int argc, char* argv[])
+{
     std::filesystem::path telemetryFilePath;
-    CLI::App app;
+    CLI::App cliParser;
 
-    app.get_formatter()->column_width(40);
-    app.description("TelemetryViewer, a GUI viewer that plots parameter values from telemetry file");
-    app.set_version_flag("-v,--version", appVersion, "Print version info and exit");
-    app.add_option("telemetry_file", telemetryFilePath, "Path to telemetry file")->required()->check(CLI::ExistingFile);
+    ConfigureCliParser(cliParser);
 
-    CLI11_PARSE(app, argc, argv);
+    cliParser.add_option(
+            "telemetry_file",
+            telemetryFilePath,
+            "Path to telemetry file")->required()->check(CLI::ExistingFile);
+
+    CLI11_PARSE(cliParser, argc, argv);
 
     std::ifstream telemetryFile { telemetryFilePath };
 
